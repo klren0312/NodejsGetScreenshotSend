a90bd61b583dce45991353cc7be6e49c5c4f8d19
ren <renzhiwei@lewell.cn>
2019-03-22 14:29:15 +0800

    format

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const nodemailer = require('nodemailer')
 const schedule = require('node-schedule')
 const fs = require('fs')
 
- 
-
-var transporter = nodemailer.createTransport({
+// 配置邮件相关
+const transporter = nodemailer.createTransport({
     host:"",
     secure:true,
     port:465,
@@ -16,44 +15,44 @@ var transporter = nodemailer.createTransport({
     debug: true // include SMTP traffic in the logs
 });
 
-var rule = new schedule.RecurrenceRule();
-	rule.second = 10;
-	var j = schedule.scheduleJob(rule,function(){
-		screenshot()
-		.then((img) => {
-			fs.writeFile('out.jpg', img,function(err){
-				if (err) {
-					throw err
-				}
-				console.log('written to out.jpg')
-			});
-	
-			var message = {
-				from:"",
-				to:"",
-				subject:"桌面截图",
-				html:'桌面截图：<img src="cid:test"/>',
-	
-				attachments:[
-					{
-						filename: 'out',
-						path: __dirname + '/out.jpg',
-						cid: 'test' // should be as unique as possible
-					}
-				]
+// 定时规则
+let rule = new schedule.RecurrenceRule();
+rule.second = 10;
+// 定时
+schedule.scheduleJob(rule, function(){
+	// 截图
+	screenshot().then((img) => {
+		fs.writeFile('out.jpg', img, function(err){
+			if (err) {
+				throw err
 			}
-			transporter.sendMail(message, (error, info) => {
-				if (error) {
-					console.log('Error occurred');
-					console.log(error.message);
-					return;
+			console.log('written to out.jpg')
+		})
+		var message = {
+			from:"",
+			to:"",
+			subject:"桌面截图",
+			html:'桌面截图：<img src="cid:test"/>',
+			attachments:[
+				{
+					filename: 'out',
+					path: __dirname + '/out.jpg',
+					cid: 'test' // should be as unique as possible
 				}
-				console.log('Message sent successfully!');
-				console.log('Server responded with "%s"', info.response);
-				transporter.close();
-			});
+			]
+		}
+		// send
+		transporter.sendMail(message, (error, info) => {
+			if (error) {
+				console.log('Error occurred')
+				console.log(error.message)
+				return
+			}
+			console.log('Message sent successfully!')
+			console.log('Server responded with "%s"', info.response)
+			transporter.close()
 		})
-		.catch((err) => {
-			throw err
-		})
-	});
+	}).catch((err) => {
+		throw err
+	})
+})
